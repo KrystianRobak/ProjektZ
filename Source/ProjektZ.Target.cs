13376a698cd59efdc95eda981fafb041584e357d
Krystian Robak <krystian.pawel.robak@gmail.com>
2024-10-02 20:23:45 +0200

    Added support for Steam and merged Branches

--- Source/ProjektZ.Target.cs
+++ Source/ProjektZ.Target.cs
@@ -8,6 +8,7 @@ public class ProjektZTarget : TargetRules
 	public ProjektZTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
+		bUsesSteam = true;
 		DefaultBuildSettings = BuildSettingsVersion.V4;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
 		ExtraModuleNames.Add("ProjektZ");
--- Source/ProjektZ/ProjektZ.Build.cs
+++ Source/ProjektZ/ProjektZ.Build.cs
@@ -18,7 +18,7 @@ public class ProjektZ : ModuleRules
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
 		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+		PrivateDependencyModuleNames.AddRange(new string[] { "OnlineSubsystem", "OnlineSubsystemSteam", "OnlineSubsystemNull" });
 
 		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
