a1254d0467c80833b8fc17ffb221478438f2cd84
Krystian Robak <krystian.pawel.robak@gmail.com>
2025-06-23 13:45:36 +0200

    Init branch 5.6

--- Source/ProjektZ/Private/Player/PC_PlayerController.cpp
+++ Source/ProjektZ/Private/Player/PC_PlayerController.cpp
@@ -67,15 +67,55 @@ void APC_PlayerController::BeginPlay()
 	}
 }
 
+//void APC_PlayerController::SetupInputComponent()
+//{
+//	Super::SetupInputComponent();
+//	UProjektZInputComponent* ProjektZInputComponent = CastChecked<UProjektZInputComponent>(InputComponent);
+//
+//
+//	ProjektZInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APC_PlayerController::Move);
+//	ProjektZInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &APC_PlayerController::Look);
+//	
+//	ProjektZInputComponent->BindAbilityActions(InputConfig, this, &ThisClass::AbilityInputTagPressed, &ThisClass::AbilityInputTagReleased, &ThisClass::AbilityInputTagHeld);
+//}
+
 void APC_PlayerController::SetupInputComponent()
 {
 	Super::SetupInputComponent();
-	UProjektZInputComponent* ProjektZInputComponent = CastChecked<UProjektZInputComponent>(InputComponent);
 
+	// The CRITICAL FIX: Ensure InputComponent is UProjektZInputComponent
+	UProjektZInputComponent* ProjektZInputComponent = Cast<UProjektZInputComponent>(InputComponent);
 
+	if (!ProjektZInputComponent)
+	{
+		// If InputComponent is NOT already a UProjektZInputComponent (which is the default for APlayerController)
+		// We create a new one and assign it.
+		ProjektZInputComponent = NewObject<UProjektZInputComponent>(this, UProjektZInputComponent::StaticClass(), TEXT("PlayerControllerCustomInputComponent"));
+		if (ProjektZInputComponent)
+		{
+			InputComponent = ProjektZInputComponent; // Assign the newly created custom input component
+			ProjektZInputComponent->RegisterComponent(); // Important: Register the component to make it active
+			UE_LOG(LogTemp, Log, TEXT("APC_PlayerController: Successfully created and set UProjektZInputComponent."));
+		}
+		else
+		{
+			// This indicates a severe issue, the game might not be playable without proper input
+			UE_LOG(LogTemp, Error, TEXT("APC_PlayerController: FAILED to create UProjektZInputComponent!"));
+			return; // Exit if we can't set up the input component
+		}
+	}
+	else
+	{
+		// This log is for debugging purposes, in case it was already the correct type (e.g. from an existing blueprint)
+		UE_LOG(LogTemp, Log, TEXT("APC_PlayerController: InputComponent was already UProjektZInputComponent."));
+	}
+
+	// Now that we are sure ProjektZInputComponent is valid and of the correct type, bind actions
+	// These are general movement and look inputs
 	ProjektZInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &APC_PlayerController::Move);
 	ProjektZInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &APC_PlayerController::Look);
-	
+
+	// These bindings use your custom BindAbilityActions for Gameplay Abilities
 	ProjektZInputComponent->BindAbilityActions(InputConfig, this, &ThisClass::AbilityInputTagPressed, &ThisClass::AbilityInputTagReleased, &ThisClass::AbilityInputTagHeld);
 }
 
@@ -116,7 +156,7 @@ void APC_PlayerController::CursorTrace()
 	FHitResult CameraHit;
 
 	FInt32Vector2 ViewPoint;
-	FRotator ViewRotation;
+	//FRotator ViewRotation;
 	GetViewportSize(ViewPoint.X, ViewPoint.Y);
 
 	ViewPoint.X = ViewPoint.X / 2;
